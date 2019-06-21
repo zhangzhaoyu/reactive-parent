6f8f150ea5f2b089fb06909b15c9a323d2e4054b
zhangzhaoyu <zhangzhaoyu0524@163.com>
2019-06-21 17:31:06 +0800

    inti reactive project

---
+++ web-flux/src/main/java/cn/cincout/reactive/webflux/domain/City.java
@@ -0,0 +1,17 @@
+package cn.cincout.reactive.webflux.domain;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * Created by zhaoyu on 19-6-21.
+ *
+ * @author zhaoyu
+ * @sine 1.8
+ */
+@Data
+public class City implements Serializable {
+    private int id;
+    private String name;
+}
---
+++ web-flux/src/main/java/cn/cincout/reactive/webflux/web/CityReactiveController.java
@@ -0,0 +1,34 @@
+package cn.cincout.reactive.webflux.web;
+
+import cn.cincout.reactive.webflux.domain.City;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by zhaoyu on 19-6-21.
+ *
+ * @author zhaoyu
+ * @sine 1.8
+ */
+@RestController
+@RequestMapping("/reactive")
+public class CityReactiveController {
+    @Resource
+    private ReactiveRedisTemplate<String, City> reactiveRedisTemplate;
+
+    @GetMapping(value = "/{id}")
+    public Mono<City> findById(@PathVariable("id") int id) {
+        String key = "CITY_" + id;
+        Mono<City> city = reactiveRedisTemplate.opsForValue().get(key);
+        return city;
+    }
+
+    @PostMapping
+    public Mono<City> saveCity(@RequestBody City city) {
+        String key = "CITY_" + city.getId();
+        return reactiveRedisTemplate.opsForValue().getAndSet(key, city);
+    }
+}

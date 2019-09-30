845005add6fd498f5b0067127cfecca6892704f7
zhangzhaoyu <zhangzhaoyu0524@163.com>
2019-09-30 17:40:15 +0800

    inti reactive project

---
+++ web-mvc/src/test/java/cn/cincout/reactive/web/mvc/WebMvcApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.cincout.reactive.web.mvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebMvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
